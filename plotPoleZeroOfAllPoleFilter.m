1565dd9dfdebbdcb572cb6f6e85d71877b1e9c82
Swrangsar Basumatary <swrangsar@aol.com>
2012-10-03 13:31:11 +0530

    pole zero plots added

--- plotPoleZeroOfAllPoleFilter.m
+++ plotPoleZeroOfAllPoleFilter.m
@@ -1,8 +1,73 @@
 close all; clear all;
+%% for /a/ in 'pani'
 
 inputFile = 'a_pani.wav';
-poleOrder = 10;
+poleOrder1 = 6;
+poleOrder2 = 10;
+num = (1);
 
-LPCoeffs = -getLPCoefficients(inputFile, poleOrder);
+LPCoeffs1 = getLPCoefficients(inputFile, poleOrder1);
+LPCoeffsFull1 = (cat(1, (1), LPCoeffs1(:)))'
+LPCoeffs2 = getLPCoefficients(inputFile, poleOrder2);
+LPCoeffsFull2 = (cat(1, (1), LPCoeffs2(:)))';
 
-LPCoeffsFull = (cat(1, [1], LPCoeffs(:)))'
+figure(100); clf;
+subplot(2, 1, 1), zplane(num, LPCoeffsFull1);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder1), ''' in ''', inputFile, ''''], 'interpreter', 'none');
+subplot(2, 1, 2), zplane(num, LPCoeffsFull2);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder2), ''' in ''', inputFile, ''''], 'interpreter', 'none');
+
+
+%% for /n/ in 'pani'
+
+inputFile = 'n_pani.wav';
+poleOrder1 = 6;
+poleOrder2 = 10;
+num = (1);
+
+LPCoeffs1 = getLPCoefficients(inputFile, poleOrder1);
+LPCoeffsFull1 = (cat(1, (1), LPCoeffs1(:)))'
+LPCoeffs2 = getLPCoefficients(inputFile, poleOrder2);
+LPCoeffsFull2 = (cat(1, (1), LPCoeffs2(:)))';
+
+figure(200); clf;
+subplot(2, 1, 1), zplane(num, LPCoeffsFull1);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder1), ''' in ''', inputFile, ''''], 'interpreter', 'none');
+subplot(2, 1, 2), zplane(num, LPCoeffsFull2);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder2), ''' in ''', inputFile, ''''], 'interpreter', 'none');
+
+%% for /I/ in 'pani'
+
+inputFile = 'i_pani.wav';
+poleOrder1 = 6;
+poleOrder2 = 10;
+num = (1);
+
+LPCoeffs1 = getLPCoefficients(inputFile, poleOrder1);
+LPCoeffsFull1 = (cat(1, (1), LPCoeffs1(:)))'
+LPCoeffs2 = getLPCoefficients(inputFile, poleOrder2);
+LPCoeffsFull2 = (cat(1, (1), LPCoeffs2(:)))';
+
+figure(300); clf;
+subplot(2, 1, 1), zplane(num, LPCoeffsFull1);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder1), ''' in ''', inputFile, ''''], 'interpreter', 'none');
+subplot(2, 1, 2), zplane(num, LPCoeffsFull2);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder2), ''' in ''', inputFile, ''''], 'interpreter', 'none');
+
+%% for /s/ in 'uska'
+
+inputFile = 's_uska.wav';
+poleOrder1 = 6;
+poleOrder2 = 10;
+num = (1);
+
+LPCoeffs1 = getLPCoefficients(inputFile, poleOrder1);
+LPCoeffsFull1 = (cat(1, (1), LPCoeffs1(:)))'
+LPCoeffs2 = getLPCoefficients(inputFile, poleOrder2);
+LPCoeffsFull2 = (cat(1, (1), LPCoeffs2(:)))';
+
+figure(400); clf;
+subplot(2, 1, 1), zplane(num, LPCoeffsFull1);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder1), ''' in ''', inputFile, ''''], 'interpreter', 'none');
+subplot(2, 1, 2), zplane(num, LPCoeffsFull2);
+title(['Pole-zero plot for all pole filter of order = ''', num2str(poleOrder2), ''' in ''', inputFile, ''''], 'interpreter', 'none');
