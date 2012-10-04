80fc3353ede7cffe63166a26cf019a5824690126
Swrangsar Basumatary <swrangsar@aol.com>
2012-10-05 03:30:46 +0530

    q3 done

--- q3/estimateSpeechParameters.m
+++ q3/estimateSpeechParameters.m
@@ -1,12 +1,19 @@
-function [pitch, LPCCoeffs, gain] = estimateSpeechParameters(inputFile)
+function [pitch, gain, LPCCoeffs] = estimateSpeechParameters(inputFile)
 
 poleOrder = 10;
 [residualSignal, fs] = getResidualSignal(inputFile, poleOrder);
-magnitudeSpectrum = abs(fft(residualSignal));
 
-% figure; plot(magnitudeSpectrum); axis tight;
+N = 2 ^ nextpow2(length(residualSignal) * 4);
+magnitudeSpectrum = abs(fft(residualSignal, N));
+w = 0:length(magnitudeSpectrum)-1;
+w = w .* 2 * pi * (1/length(magnitudeSpectrum));
 
-pitch = getPitch(inputFile, fs)
+figure; plot(w, magnitudeSpectrum); axis tight;
+title(['Magnitude spectrum of residual signal for ''', inputFile, ...
+    ''''], 'interpreter', 'none');
+xlabel('''w'' in radians');
+
+pitch = getPitch(inputFile, fs);
 LPCCoeffs = getLPCCoefficients(inputFile, poleOrder);
 gain = getGain(inputFile, poleOrder);
 
@@ -121,26 +128,44 @@ end
 
 end
 
+%% highlight the peaks of the signal retaining their algebraic sign 
+function highlightedSignal = highlightSignal(signal)
+
+highlightedSignal = signal .^ 3;
+
+end
+
 %% get pitch of the windowed signal using harmonic product spectrum
 
 function pitch = getPitch(inputFile, fs)
+% We calculate the pitch by downsampling the narrowband spectrum by integer
+% values and add them to the original spectrum. At F0, they give a peak
+% because the peaks of other harmonics add up at F0. We take the second
+% peak of the final spectrum because there could be other peaks in the
+% spectrum including one at f = 0 Hz. This way of getting the pitch is not
+% perfect because the pitch values change with the N-point of FFT
+% considered.
+
 
 windowedSignal = getWindowedSignal(inputFile);
-magnitudeSpectrum = abs(fft(windowedSignal, length(windowedSignal)));
+windowedSignal = highlightSignal(windowedSignal);
+
+N = 2 ^ (nextpow2(length(windowedSignal) * 4));
+magnitudeSpectrum = abs(fft(windowedSignal, N));
 frequencyFactor = fs ./ length(magnitudeSpectrum);
 magnitudeSpectrum = magnitudeSpectrum(1:round(length(magnitudeSpectrum)/2));
 spectralPeaks = magnitudeSpectrum;
 
-for k = 2:length(magnitudeSpectrum)-1
+for k = 2:length(magnitudeSpectrum)
     spectralPeaks = spectralPeaks + getDownsampledSignal(magnitudeSpectrum, k);
 end
 
-spectralPeaks(1) = 0;
-
-maxIndex = find(spectralPeaks == max(spectralPeaks));
+% spectralPeaks(1) = 0;
+peaks = findpeaks(spectralPeaks);
+maxIndex = find(spectralPeaks == peaks(2));
 maxIndex = maxIndex(1);
 
-figure, stem(spectralPeaks);
+% figure, stem(spectralPeaks); axis tight;
 
 pitch = (maxIndex-1) * frequencyFactor;
 
--- q3/estimateSpeechParametersScript.m
+++ q3/estimateSpeechParametersScript.m
@@ -2,10 +2,10 @@ close all; clear all;
 addpath /Users/swrangsarbasumatary/Desktop/speechProcessingProject/q2ab/
 addpath /Users/swrangsarbasumatary/Desktop/speechProcessingProject/
 
-estimateSpeechParameters('a_pani.wav');
-estimateSpeechParameters('n_pani.wav');
-estimateSpeechParameters('i_pani.wav');
-estimateSpeechParameters('s_uska.wav');
+[pitch1, gain1, LPCCoeffs1] = estimateSpeechParameters('a_pani.wav');
+[pitch2, gain2, LPCCoeffs2] = estimateSpeechParameters('n_pani.wav');
+[pitch3, gain3, LPCCoeffs3] = estimateSpeechParameters('i_pani.wav');
+[pitch4, gain4, LPCCoeffs4] = estimateSpeechParameters('s_uska.wav')
 
 
 rmpath /Users/swrangsarbasumatary/Desktop/speechProcessingProject/
