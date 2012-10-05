34598379362e5c229c213eec2bd36804b8c4fc5e
Swrangsar Basumatary <swrangsar@aol.com>
2012-10-05 09:40:25 +0530

    finalized for submission

---
+++ q1/getNarrowbandSpectrumScript.m
@@ -0,0 +1,10 @@
+close all; clear all;
+
+addpath /Users/swrangsarbasumatary/Desktop/speechProcessingProject/
+
+getNarrowbandSpectrum('a_pani.wav');
+getNarrowbandSpectrum('n_pani.wav');
+getNarrowbandSpectrum('i_pani.wav');
+getNarrowbandSpectrum('s_uska.wav');
+
+rmpath /Users/swrangsarbasumatary/Desktop/speechProcessingProject/
